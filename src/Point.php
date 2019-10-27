ea78ee01898040423fc5bf07948778b43f0f42e2
Dmitry Kopytine <root@intminds.com>
2019-10-27 21:32:15 +0000

    Point class - done.

--- src/Point.php
+++ src/Point.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Intminds\GPS;
 
-class Point
+class Point implements \IteratorAggregate, \ArrayAccess
 {
     /**
      * @var float
@@ -22,16 +22,76 @@ class Point
      */
     public $time;
     /**
-     * @var float|null
+     * @var array
      */
-    public $distance;
+    public $props = [];
 
-    public function __construct(float $lat, float $lng, ?float $alt = 0, int $time = null, float $distance = null)
+    public function __construct(float $lat, float $lng, ?float $alt = null, int $time = null)
     {
         $this->lat = $lat;
         $this->lng = $lng;
-        $this->alt = is_null($alt) ? 0 : $alt;
+        $this->alt = $alt;
         $this->time = $time;
-        $this->distance = $distance;
+    }
+
+    /**
+     * @return \Traversable
+     */
+    public function getIterator()
+    {
+        yield "lat" => $this->lat;
+        yield "lng" => $this->lng;
+        yield "alt" => $this->alt;
+        yield "time" => $this->time;
+        yield from $this->props;
+    }
+
+    public function offsetExists($offset): bool
+    {
+        return in_array($offset, ["lat", "lng", "alt", "time"]) || isset($this->props[$offset]) && !is_null($this->props[$offset]);
+    }
+
+    public function offsetGet($offset)
+    {
+        switch ($offset) {
+            case "lat":
+                return $this->lat;
+            case "lng":
+                return $this->lng;
+            case "alt":
+                return $this->alt;
+            case "time":
+                return $this->time;
+            default:
+                return $this->props[$offset] ?? null;
+        }
+    }
+
+    public function offsetSet($offset, $value): void
+    {
+        switch ($offset) {
+            case "lat":
+                $this->lat = $value;
+                break;
+            case "lng":
+                $this->lng = $value;
+                break;
+            case "alt":
+                $this->alt = $value;
+                break;
+            case "time":
+                $this->time = $value;
+                break;
+            default:
+                $this->props[$offset] = $value;
+        }
+    }
+
+    public function offsetUnset($offset): void
+    {
+        if (in_array($offset, ["lat", "lng", "alt", "time"])) {
+            throw new \OutOfRangeException();
+        }
+        unset($this->props[$offset]);
     }
 }
--- tests/PointTest.php
+++ tests/PointTest.php
@@ -7,9 +7,48 @@ use PHPUnit\Framework\TestCase;
 
 final class PointTest extends TestCase
 {
-    public function testBasic()
+    public function testConstructor()
     {
+        $t = time();
+        $p = new Point(4.0, 5.0, 6.0, $t);
+        $this->assertEqualsWithDelta(4.0, $p->lat, 0.01);
+        $this->assertEqualsWithDelta(5.0, $p->lng, 0.01);
+        $this->assertEqualsWithDelta(6.0, $p->alt, 0.01);
+        $this->assertSame($t, $p->time);
+
         $p = new Point(2.0, 3.0);
-        $this->assertSame($p->lat, 2.0);
+        $this->assertEqualsWithDelta(2.0, $p->lat, 0.01);
+        $this->assertNull($p->alt);
+        $this->assertNull($p->time);
+    }
+
+    public function testArrayAccess()
+    {
+        $t = time();
+        $p = new Point(4.0, 5.0, 6.0, $t);
+        $p["distance"] = 1234;
+        $this->assertEquals(1234, $p["distance"]);
+        $this->assertNull($p["XXX"]);
+        $this->assertTrue(isset($p["alt"]));
+        $this->assertTrue(isset($p["distance"]));
+        $this->assertFalse(isset($p["XXX"]));
+        unset($p["XXX"]); // Should not throw
+        unset($p["distance"]);
+        $this->assertNull($p["distance"]);
+        $this->expectException(\OutOfRangeException::class);
+        unset($p["lat"]);
+    }
+
+    public function testTraversable()
+    {
+        $p = new Point(4.0, 5.0, 6.0);
+        $p["g"] = "G";
+        $this->assertSame([
+            "lat" => 4.0,
+            "lng" => 5.0,
+            "alt" => 6.0,
+            "time" => null,
+            "g" => "G",
+        ], iterator_to_array($p));
     }
 }
